08324bd9c7e570ebeb5b340cada2feddb5d15a57
Skylar4 <95008499+Skylar4@users.noreply.github.com>
2023-12-07 07:00:11 -0700

    updated JavaScript sintax

--- scripts/booking.js
+++ scripts/booking.js
@@ -17,7 +17,7 @@ const friday = document.getElementById("friday");
 //full and half day elements
 const full = document.getElementById("full");
 const half = document.getElementById("half");
-const clear = document.getElementById("clear-button")
+const clear = document.getElementById("clear-button");
 
 
 
@@ -27,7 +27,7 @@ const clear = document.getElementById("clear-button")
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful he
 function changeColor(day){
     if (day.classList.contains("clicked")){
-        day.classList.remove("clicked")
+        day.classList.remove("clicked");
         numberOfDay -= 1;
     }
     else{
@@ -37,23 +37,23 @@ function changeColor(day){
 }
 monday.addEventListener("click", function(){
     changeColor(monday);
-    calculate()
+    calculate();
  });
  tuesday.addEventListener("click", function(){
     changeColor(tuesday);
-    calculate()
+    calculate();
  });
  wendsday.addEventListener("click", function(){
     changeColor(wendsday);
-    calculate()
+    calculate();
  });
  thursday.addEventListener("click", function(){
     changeColor(thursday);
-    calculate()
+    calculate();
  });
  friday.addEventListener("click", function(){
     changeColor(friday);
-    calculate()
+    calculate();
  });
 
 
@@ -64,18 +64,18 @@ monday.addEventListener("click", function(){
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
 function clearAll(){
-    list_of_days = [monday,tuesday,wendsday,thursday,friday]
+    let list_of_days = [monday,tuesday,wendsday,thursday,friday];
     for (let day of list_of_days){
-        day.classList.remove("clicked")
+        day.classList.remove("clicked");
     }
-    full.classList.add("clicked")
-    half.classList.remove("clicked")
-    numberOfDay = 0
+    full.classList.add("clicked");
+    half.classList.remove("clicked");
+    numberOfDay = 0;
 }
 clear.addEventListener("click", function(){
     clearAll();
     calculate();
-})
+});
 
 
 
@@ -84,31 +84,31 @@ clear.addEventListener("click", function(){
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
 half.addEventListener("click", function(){
-    changeColor(half)
-    changeColor(full)
-    calculate()
-})
+    changeColor(half);
+    changeColor(full);
+    calculate();
+});
 
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
-full.addEventListener("click",function(){
-    changeColor(full)
-    changeColor(half)
-    calculate()
-})
+    full.addEventListener("click",function(){
+        changeColor(full);
+        changeColor(half);
+        calculate();
+    });
 
 
 
-/********* calculate *********/
-// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
+    /********* calculate *********/
+    // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-function calculate(){
-    if (full.classList.contains("clicked")){
-        costPerDay.innerHTML = numberOfDay * 35
+    function calculate(){
+        if (full.classList.contains("clicked")){
+            costPerDay.innerHTML = numberOfDay * 35;
+        }
+        if (half.classList.contains("clicked")){
+            costPerDay.innerHTML = numberOfDay * 20;
+        }
     }
-    if (half.classList.contains("clicked")){
-        costPerDay.innerHTML = numberOfDay * 20
-    }
-}
